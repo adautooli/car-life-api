300d94703422576618b38f6cc413fd015953e2b0
adautooli <adauto.oliveira@me.com>
2025-04-14 23:25:13 -0300

    Rich response for endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -11,16 +11,16 @@ router.post('/register', async (req, res) => {
 
   try {
     const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ msg: 'Email já cadastrado' });
+    if (existingUser) return res.status(400).json({status: false, msg: 'Email já cadastrado' });
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = new User({ fullName, email, password: hashedPassword });
     await user.save();
 
-    res.status(201).json({ msg: 'Usuário cadastrado com sucesso' });
+    res.status(201).json({ status: true ,msg: 'Usuário cadastrado com sucesso' });
   } catch (err) {
-    res.status(500).json({ msg: 'Erro no servidor' });
+    res.status(500).json({ status: false, msg: 'Erro no servidor' });
   }
 });
 
@@ -30,30 +30,30 @@ router.post('/login', async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ msg: 'Usuário não encontrado' });
+    if (!user) return res.status(400).json({status: false, msg: 'Usuário não encontrado' });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ msg: 'Senha incorreta' });
+    if (!isMatch) return res.status(401).json({status: false, msg: 'Senha incorreta' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '2h' });
 
-    res.json({ token });
+    res.json({status: true, token });
   } catch (err) {
-    res.status(500).json({ msg: 'Erro no servidor' });
+    res.status(500).json({status: false, msg: 'Erro no servidor' });
   }
 });
 
 // Dados do usuário autenticado
 router.get('/me', async (req, res) => {
   const token = req.headers['authorization']?.split(' ')[1];
-  if (!token) return res.status(401).json({ msg: 'Token não fornecido' });
+  if (!token) return res.status(401).json({ status: false, msg: 'Token não fornecido' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.id).select('-password');
-    res.json(user);
+    res.json({ status: true, user });
   } catch (err) {
-    res.status(401).json({ msg: 'Token inválido' });
+    res.status(401).json({status: false, msg: 'Token inválido ou expirado por favor faça login novamente' });
   }
 });
 
