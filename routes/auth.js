53bdd9b5f31935df7d4d58860489cbcc48e216a9
Adauto Oliveira <adauto.oliveira@me.com>
2025-10-27 19:40:23 -0300

    Incluindo model Car no auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Car = require('../models/Car');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authenticate = require('../middleware/auth');
